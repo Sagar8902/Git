b949306c9bc9415e65484bd90ab1f1e186caf9ff
Sagar_Soni <100688472+Sagar8902@users.noreply.github.com>
2024-05-15 10:31:39 +0530

    Add files via upload

---
+++ Foundation.py
@@ -0,0 +1,37 @@
+
+
+# i = 1
+#
+# while i <= 20:
+#     if i%2 == 0:
+#         print(i)
+#     else:
+#         pass
+#
+#     i += 1
+#
+# print("Task has done")
+
+
+# i = 1
+#
+# while i <= 10:
+#     if(i == 7):
+#         continue
+#     else:
+#         print(i)
+#     i += 1
+
+# num = int(input("Enter number here: "))
+#
+# if(num%2 == 0):
+#     print("Number is not prime")
+# else:
+#     print("Number is prime")
+
+
+list = [6,5,4,8,4,5]
+
+list.reverse()
+
+
