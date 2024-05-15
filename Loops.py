31091a0d0b87a2970716d1cfbd329c5656479d74
Sagar_Soni <100688472+Sagar8902@users.noreply.github.com>
2024-05-15 10:32:25 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,46 @@
+# i = 1
+#
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+##################
+
+i = 1
+
+while i <= 10:
+    print(i*"*")
+    i += 1
+
+i = 10
+
+while i >= 1:
+    print(i*"*")
+    i -= 1
+
+
+list = [1,2,6,7,9,2,5,8,6]
+x = 0
+
+for l in list:
+    if(l == 6):
+        print("Found it",x)
+    x += 1
+    print(l)
+
+
+for i in range(11):
+    print(i * "*")
+
+
+
+
+
+
+
+
+
+
+
+
+
