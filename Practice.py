4a087392bdf7e11d701467b2c7e3f4d13f15db57
Sagar_Soni <100688472+Sagar8902@users.noreply.github.com>
2024-05-15 10:32:38 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,17 @@
+print("hello")
+
+var = "sagar"
+
+list =[1,2,3,4,5]
+
+tup = (1,2,3,4,5)
+
+dict = {
+    "name" : "sagar",
+    "age" : 22,
+    "position" : "data analyst",
+    "location" : "surat"
+}
+
+set = {1,2,3,4,5,1,2,3,2,2,3}
+
